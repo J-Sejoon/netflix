34b8bc6763f1ed483008ddbc295e076a1fa5c3fa
J-Sejoon <110226533+J-Sejoon@users.noreply.github.com>
2023-05-11 11:02:21 +0900

    movieReducer.js

--- src/redux/reducers/movieReducer.js
+++ src/redux/reducers/movieReducer.js
@@ -11,13 +11,10 @@ let initialState = {
 	trailerVideo: {},
 };
 
-//createSlice - 리듀서를 만들어줌, 객체를 매개변수로, 3개 필요(name,initialState,reducers)
 const movieSlice = createSlice({
 	name: "movie", //액션 네임을 만들어줌
 	initialState,
 	reducers: {
-		//기존에 if elseif, switch역할, 함수(2개의 매개변수를 받음)
-		//로딩시작
 		getMoviesRequest(state, action) {
 			state.loading = true;
 		},
